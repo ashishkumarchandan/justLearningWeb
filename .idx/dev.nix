1b83934ea354369a9b0cf447367b690900f4ac08
aaassshhhiiissshhh111 <aaassshhhiiissshhh111@gmail.com>
2024-05-18 10:06:00 +0000

    ok

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,18 +3,16 @@
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
-
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
     # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
-    pkgs.neofetch
+    pkgs.python3
     pkgs.htop
+    pkgs.neofetch
+    pkgs.nodejs_20
+    pkgs.gcc
+    pkgs.cmake
   ];
-
   # Sets environment variables in the workspace
   env = {};
   idx = {
@@ -22,35 +20,14 @@
     extensions = [
       # "vscodevim.vim"
     ];
-
-    # Enable previews
+    # Enable previews and customize configuration
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+        web = {
+          command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
+          manager = "web";
+        };
       };
     };
   };
